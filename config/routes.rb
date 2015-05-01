ae1e4b9f8ee494255fb4d12d87705ac61636eded
Daniel Uribe <duribe8@gmail.com>
2015-04-30 22:26:38 -0700

    Added authorization with Bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get "log_out" => 'sessions#destroy', :as => "log_out"
+  get "log_in" => 'sessions#new', :as => "log_in"
+  get "sign_up" => 'users#new', :as => "sign_up"
+
+
   get 'welcome/index'
   get 'welcome/about'
 
-  root to: 'welcome#index'
-
+  # root to: 'welcome#index'
+  root to: "users#new"
+  resources :users
+  resources :sessions
  
 end
